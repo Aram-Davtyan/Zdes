9c6a50038208895a8045b8f1447897402c8483df
Aram-Davtyan <90602705+Aram-Davtyan@users.noreply.github.com>
2023-05-12 19:40:13 +0300

    Add files via upload

---
+++ 5.1б/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+double F(double x,double y)
+{
+    return 2*x;
+}
+
+    void NewF(double x, double y)
+    {
+        double  nx, ny;
+
+        double hx = 0.1;
+        for (nx = x; nx <= 1.; nx += hx)
+        {
+            ny = y + hx * F(x, y);
+
+        	    ofstream myfile("сoord.txt", ios::app);
+
+	            if (!myfile)  //если файл неоткрылся
+	            {
+		            cout << "Sorry" << endl;
+		            system("pause");
+	            }
+	            else 
+		            myfile << nx << "       " << ny << endl;
+
+
+        myfile.close();
+
+        x = nx;
+        y = ny;
+        }
+    }
+
+
+
+int main()
+{
+    NewF(0,0);
+}
+
